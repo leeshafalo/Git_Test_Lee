f337ac7cea7f563b1e3f75669a47dd7ffa6862c1
LeeDaye <chgksmf06@gmail.com>
2015-12-05 00:24:47 +0900

    sum module insert

--- git_lee.c
+++ git_lee.c
@@ -6,5 +6,16 @@ int main(){
 
 	scanf("%d %d", &a, &b);
 
+	printf("%d", sum(a, b));
+
 	return 0;
+}
+
+int sum(int a, int b){
+	int sum = 0;
+	while (a <= b){
+		sum += a;
+		a++;
+	}
+	return sum;
 }
